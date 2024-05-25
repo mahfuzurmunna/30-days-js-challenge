dfc13cd2a94c578ccb227ec7d2cc30f213a5c9b9
Mahfuzur Rahman Munna <mahfuzurrm07@gmail.com>
2024-05-25 23:34:47 +0600

    string methods

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -2,5 +2,13 @@ const gameName = new String("Mahfuzur");
 
 console.log(gameName[0]);
 
-console.log(gameName.__proto__);
+console.log(gameName.indexOf("r"));
 // thing isnt easy as it seems.
+
+console.log(gameName.concat(" munna"));
+
+console.log(gameName.toLowerCase());
+
+const url = "https://mahfuzur.com/munna%20rahman";
+
+console.log(gameName.replace("Mahfuzur", "Valorant"));
