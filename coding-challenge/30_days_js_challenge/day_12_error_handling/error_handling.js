a05ff1bc5b1f8348638fdfb916727ebe9532be32
Mahfuzur Rahman Munna <mahfuzurrm07@gmail.com>
2025-04-21 23:53:01 +0600

    error

--- coding-challenge/30_days_js_challenge/day_12_error_handling/error_handling.js
+++ coding-challenge/30_days_js_challenge/day_12_error_handling/error_handling.js
@@ -31,7 +31,38 @@ try {
   console.log("Finally we get a result");
 }
 
-
-class customError {
-    
+class CustomError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "CustomError";
+  }
 }
+
+function validateString(input) {
+  if (input.trim() === "") {
+    throw new CustomError("input string cannot be empty");
+  }
+  console.log("input is valid");
+}
+
+try {
+  validateString("");
+} catch (error) {
+  console.error(`${error.message}`);
+}
+
+class EmptyInputError extends Error {
+  constructor(message) {
+    super(message);
+    this.name = "emptyInputError";
+  }
+}
+
+function validatesString(input) {
+  if (input === "") {
+    throw new EmptyInputError("Input cannot be empty");
+  }
+  console.log("Input is valid");
+}
+
+
